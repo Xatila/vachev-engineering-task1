8c40a71f66ba4072ad7c4c59960ca7f8d6a1d9f0
Kevin Baliev <61345825+Xatila@users.noreply.github.com>
2024-06-02 14:40:57 +0300

    Background component and set alias paths in tsconfig.json

---
+++ src/app/components/background/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+import styles from "@components/background/index.module.scss";
+
+const Background: FC = () => <div className={styles.background}></div>;
+
+export default Background;
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,1 +1,4 @@
 export { default as CardView } from "./card-view";
+export { default as Cards } from "./cards";
+export { default as Background } from "./background";
+export { default as Title } from "./title";
